f1829aabfafd2d596c00d990fcf971caede09bbe
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-02-22 16:31:00 +0800

    Create findLinkInput.js

---
+++ findLinkInput.js
@@ -0,0 +1,31 @@
+function findInput(name,succeed,error){try{
+eval(name+"=undefined")
+var path=location.href
+if(path.search("\\?")!==-1){
+var inputContent=path.split("?")[1]
+if(inputContent.search("&")!==-1) { 
+inputContent=inputContent.split("&") 
+var i=0;/*/SUM=7/*/
+for(;i<inputContent.length;i++){
+if(eval("/"+name+"=/.test(inputContent[i])")){
+eval(name+"=inputContent[i].replace("+'"'+name+"=","");
+break;
+ }
+}
+} else {
+if(inputContent.search(eval('/'+name+'=/')!==-1){
+eval(name+'=inputContent.replace(/'+name+'=/,"")'
+ }
+}
+  }
+
+if(name!==undefined){
+if(typeof(succeed)=="function") {return succeed()} else
+if(typeof(succeed)=="function") {return eval(succeed)} else
+return eval(name);
+ } else {
+if(typeof(error)=="function") {return error()} else
+if(typeof(error)=="function") {return eval(error)} else
+return eval(name);
+ }
+}catch(err){alert(err)}})()
